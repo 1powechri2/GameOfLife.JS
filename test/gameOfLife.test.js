97d7b8980bdbdbee18503114d23df0f6d632a260
Chris Powell <chris.ian.powell@gmail.com>
2019-06-28 12:28:37 -0600

    Adds tests for creating first generation and creating and empty world for subsequent generations

--- test/gameOfLife.test.js
+++ test/gameOfLife.test.js
@@ -1,12 +1,46 @@
 const GameOfLife = require('../lib/gameOfLife')
+const pry = require('pryjs')
+
+const gameOfLife = new GameOfLife(4, 5);
 
 test('it can be intialized', () => {
-  const gofClass = new GameOfLife(5, 4);
-  expect(gofClass.width).toBe(5);
-  expect(gofClass.height).toBe(4);
+  expect(gameOfLife.height).toBe(4);
+  expect(gameOfLife.width).toBe(5);
 })
 
-test('it can return a two dimensional array from the height and width',
-    () => {
-      
-    })
+test('it can return a two dimensional array from the height and width\
+containg binary values', () => {
+  expect(gameOfLife.world()).toBeInstanceOf(Array)
+
+  expect(gameOfLife.world()[0]).toBeInstanceOf(Array)
+  expect(gameOfLife.world()[1]).toBeInstanceOf(Array)
+  expect(gameOfLife.world()[2]).toBeInstanceOf(Array)
+  expect(gameOfLife.world()[3]).toBeInstanceOf(Array)
+
+  expect(gameOfLife.world()[0][1]).toBeLessThan(2)
+  expect(gameOfLife.world()[0][1]).toBeGreaterThan(-1)
+  expect(gameOfLife.world()[1][2]).toBeLessThan(2)
+  expect(gameOfLife.world()[1][2]).toBeGreaterThan(-1)
+  expect(gameOfLife.world()[2][3]).toBeLessThan(2)
+  expect(gameOfLife.world()[2][3]).toBeGreaterThan(-1)
+  expect(gameOfLife.world()[3][4]).toBeLessThan(2)
+  expect(gameOfLife.world()[3][4]).toBeGreaterThan(-1)
+})
+
+test('it can create an empty world to fill with next generation', () => {
+  expect(gameOfLife.emptyWorld()).toBeInstanceOf(Array)
+
+  expect(gameOfLife.emptyWorld()[0]).toBeInstanceOf(Array)
+  expect(gameOfLife.emptyWorld()[1]).toBeInstanceOf(Array)
+  expect(gameOfLife.emptyWorld()[2]).toBeInstanceOf(Array)
+  expect(gameOfLife.emptyWorld()[3]).toBeInstanceOf(Array)
+
+  expect(gameOfLife.emptyWorld()[0][1]).toBeNull()
+  expect(gameOfLife.emptyWorld()[0][1]).toBeNull()
+  expect(gameOfLife.emptyWorld()[1][2]).toBeNull()
+  expect(gameOfLife.emptyWorld()[1][2]).toBeNull()
+  expect(gameOfLife.emptyWorld()[2][3]).toBeNull()
+  expect(gameOfLife.emptyWorld()[2][3]).toBeNull()
+  expect(gameOfLife.emptyWorld()[3][4]).toBeNull()
+  expect(gameOfLife.emptyWorld()[3][4]).toBeNull()
+})
